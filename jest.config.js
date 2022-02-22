ef15f1c676b08b5570c9da87aa6d821c13a544c6
Robson Maia <robsonmvieira@gmail.com>
2022-02-22 13:39:58 -0300

    🧪add jest test config

---
+++ jest.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  collectCoverageFrom: [
+    '<rootDir>/tests/**/*.ts',
+    '**/tests/**',
+    '<rootDir>/tests/**/*.spec.ts',
+    '<rootDir>/**/*.spec.ts',
+    '<rootDir>/tests/**/*.test.ts',
+    '!<rootDir>/tests/**/*index.ts',
+    '!<rootDir>/tests/**/*app.module.ts',
+    '!<rootDir>/tests/**/*main.ts',
+    '!<rootDir>/tests/**/*Result.ts',
+    '!<rootDir>/tests/**/*interface.ts',
+    '!<rootDir>/tests/**/*env.ts',
+    '!<rootDir>/tests/Shared/*',
+    '!<rootDir>/tests/Shared/events/*'
+  ],
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['\\\\node_modules\\\\'],
+  coverageProvider: 'v8',
+  roots: ['<rootDir>/tests'],
+  testEnvironment: 'node',
+  testPathIgnorePatterns: ['\\\\node_modules\\\\'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest'
+  },
+  testRegex: '.*\\.spec\\.ts$',
+  moduleFileExtensions: ['js', 'json', 'ts']
+}
