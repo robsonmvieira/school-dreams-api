8f4b45ace138430faf44f20d9bad42b0999d6242
Robson Maia <robsonmvieira@gmail.com>
2022-02-22 12:50:45 -0300

    ✨add password value Object

---
+++ src/modules/domain/shared/value-objects/password.vo.ts
@@ -0,0 +1,31 @@
+import { ValueObject } from '../../core/based-classes/value-object'
+import { Result } from '../../core/based-classes/result'
+export type PasswordValueObjectProps = {
+  value: string
+}
+
+export class PasswordValueObject extends ValueObject<PasswordValueObjectProps> {
+  private static readonly minimalPasswordLength = 6
+  private static readonly maximumPasswordLength = 10
+  private constructor(props: PasswordValueObjectProps) {
+    super(props)
+  }
+
+  get value(): string {
+    return this.props.value
+  }
+  public static create(newPassword: string): Result<PasswordValueObject> {
+    const emailLength = newPassword.length
+    const isValidSize =
+      emailLength >= this.minimalPasswordLength &&
+      emailLength <= this.maximumPasswordLength
+    if (!isValidSize) {
+      return Result.fail<PasswordValueObject>(
+        `You need provider the password size between ${this.minimalPasswordLength} and ${this.maximumPasswordLength}`
+      )
+    }
+    return Result.ok<PasswordValueObject>(
+      new PasswordValueObject({ value: newPassword })
+    )
+  }
+}
