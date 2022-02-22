74cb94e9fe1a2610b6ea08e3552d70caadb9cf87
Robson Maia <robsonmvieira@gmail.com>
2022-02-22 14:24:23 -0300

    ✨ add app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,26 @@
 import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { join } from 'path'
+import { AdminModule } from '@domain/admin/admin.module'
+import { ClientModule } from '@domain/client/client.module'
+import { CoreModule } from '@domain/core/core.module'
+import { ParentsModule } from '@domain/parents/parents.module'
+import { StudentModule } from '@domain/student/student.module'
+import { TeacherModule } from '@domain/teacher/teacher.module'
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: []
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql')
+    }),
+    AdminModule,
+    ClientModule,
+    CoreModule,
+    StudentModule,
+    TeacherModule,
+    ParentsModule
+  ]
 })
 export class AppModule {}
