c6cba3715ab9807bdb2ffce84490a76f9e1cae61
Robson Maia <robsonmvieira@gmail.com>
2022-02-22 19:20:20 -0300

    🧪 update jest config

--- jest.config.js
+++ jest.config.js
@@ -24,5 +24,18 @@ module.exports = {
     '^.+\\.ts$': 'ts-jest'
   },
   testRegex: '.*\\.spec\\.ts$',
-  moduleFileExtensions: ['js', 'json', 'ts']
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/modules/$1',
+    '@root/(.*)': '<rootDir>/src/$1',
+    '@domain/(.*)': '<rootDir>/src/modules/domain/$1',
+    '@shared/(.*)': '<rootDir>/src/modules/domain/shared/$1',
+    '@infra/(.*)': '<rootDir>/src/infra/$1'
+  }
 }
+// '@shared-core/(.*)': '<rootDir>/src/domain/shared/core/$1',
+// '@shared-common/(.*)': '<rootDir>/src/domain/shared/common/$1',
+//     '@repo/(.*)': '<rootDir>/src/repo/$1',
+//     '@app/(.*)': '<rootDir>/src/application/$1',
+//     '@config/(.*)': '<rootDir>/src/config/$1',
+//     '@utils/(.*)': '<rootDir>/src/utils/$1'
