8f178190f538d264898fd2d0a8e718d850ec113b
Robson Maia <robsonmvieira@gmail.com>
2022-02-24 19:25:22 -0300

    ♻️ update default contract

--- src/core/ports/repository.ports.ts
+++ src/core/ports/repository.ports.ts
@@ -5,20 +5,25 @@ export interface Save<Entity> {
 export interface FindMany<Entity, EntityProps> {
   findMany(entity: EntityProps): Promise<Entity[]>
 }
-export interface FindOne<Entity, EntityProps> {
-  findOneOrThrow(params: EntityProps): Promise<Entity>
+export interface FindOne<Entity> {
+  findOneOrThrow(id: string): Promise<Entity>
+}
+
+export interface Update<Entity, EntityProps> {
+  update(id: string, params: EntityProps): Promise<Entity>
 }
 
 export interface FindOneById<Entity> {
   findOneByIdOrThrow(id: string): Promise<Entity>
 }
 export interface DeleteOne<Entity> {
-  delete(entity: Entity): Promise<Entity>
+  delete(predicate: string): Promise<Entity>
 }
 
 export interface RepositoryPort<Entity, EntityProps>
   extends Save<Entity>,
-    FindOne<Entity, EntityProps>,
+    FindOne<Entity>,
+    Update<Entity, EntityProps>,
     FindOneById<Entity>,
     FindMany<Entity, EntityProps>,
     DeleteOne<Entity> {}
