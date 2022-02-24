afd39cc6694ee1634c3b5aa2c1079251e1fb99f6
Robson Maia <robsonmvieira@gmail.com>
2022-02-24 19:26:17 -0300

    ✨add  new repository provider

--- src/modules/admin/categories/categories.module.ts
+++ src/modules/admin/categories/categories.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common'
 import { CategoriesResolver } from './categories.resolver'
+import { CategoryRepository } from './database'
 import { CategoriesApiService, CategoriesService } from './services'
 
 @Module({
-  providers: [CategoriesResolver, CategoriesApiService, CategoriesService]
+  providers: [
+    CategoriesResolver,
+    CategoriesApiService,
+    CategoriesService,
+    {
+      provide: 'CategoryRepositoryPort',
+      useClass: CategoryRepository
+    }
+  ]
 })
 export class CategoriesModule {}
