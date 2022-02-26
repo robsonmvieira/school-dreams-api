64d37721d34161e1250830eaf3aa0ea14d121862
Robson Maia <robsonmvieira@gmail.com>
2022-02-26 00:56:08 -0300

    ✨add prisma service

---
+++ src/infra/factories/prisma.connection.ts
@@ -0,0 +1,23 @@
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common'
+import { PrismaClient } from '@prisma/client'
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super()
+  }
+  async onModuleInit() {
+    await this.$connect()
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    this.$on('beforeExit', async () => {
+      await app.close()
+      console.log('Database connection has been finished.')
+    })
+  }
+
+  async onModuleDestroy() {
+    await this.$disconnect()
+  }
+}
