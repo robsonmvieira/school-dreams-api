c404493fc829bf94bc03510c936cdd6828378f9e
Robson Maia <robsonmvieira@gmail.com>
2022-02-26 22:19:27 -0300

    ✨register create graphqlResolver

--- src/modules/admin/categories/categories.module.ts
+++ src/modules/admin/categories/categories.module.ts
@@ -1,15 +1,20 @@
 import { Module } from '@nestjs/common'
 
 import { CategoryRepository } from './database'
-import { FindCategoriesGraphQlResolver } from '@modules/admin/categories/database/queries'
-import { CategoriesApiService, CategoriesService } from './services'
+import { FindCategoriesGraphQlResolver } from '@modules/admin/categories/queries/find-categories'
+import {
+  CategoriesApiService,
+  CategoriesService
+} from '@modules/admin/categories/services'
 import { PrismaService } from '@infra/factories/prisma.connection'
+import { CreateCategoryGraphQlResolver } from '@modules/admin/categories/commands/create-category'
 
 @Module({
   imports: [],
   providers: [
     PrismaService,
     FindCategoriesGraphQlResolver,
+    CreateCategoryGraphQlResolver,
     CategoriesApiService,
     CategoriesService,
     {
