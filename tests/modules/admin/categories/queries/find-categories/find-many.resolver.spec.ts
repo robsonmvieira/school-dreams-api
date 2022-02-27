913a307daca7aedb27194a3ed1d0e669b370ebe3
Robson Maia <robsonmvieira@gmail.com>
2022-02-26 22:35:19 -0300

    ✅add category related tests

---
+++ tests/modules/admin/categories/queries/find-categories/find-many.resolver.spec.ts
@@ -0,0 +1,43 @@
+import { Category } from '@modules/admin/categories/entities'
+import { FindCategoriesGraphQlResolver } from '@modules/admin/categories/queries/find-categories'
+import { CategoriesService } from '@modules/admin/categories/services'
+import { Test, TestingModule } from '@nestjs/testing'
+
+describe('FindCategoriesGraphQlResolver', () => {
+  let resolver: FindCategoriesGraphQlResolver
+
+  const mockResult: Category[] = [
+    {
+      id: 'any_id',
+      title: 'any_title',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }
+  ]
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        FindCategoriesGraphQlResolver,
+        {
+          provide: CategoriesService,
+          useValue: {
+            findAll: jest.fn().mockResolvedValue(mockResult)
+          }
+        }
+      ]
+    }).compile()
+
+    resolver = module.get<FindCategoriesGraphQlResolver>(
+      FindCategoriesGraphQlResolver
+    )
+  })
+
+  it('should be defined', () => {
+    expect(resolver).toBeDefined()
+  })
+
+  it('should list categories', async () => {
+    const result = await resolver.findAll()
+    expect(result).toEqual(mockResult)
+  })
+})
